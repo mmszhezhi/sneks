a0dc60cc8bee6f8e5285c4df666da5270ddeb3f1
mmszhezhi <mmszhezhi@gmail.com>
2022-09-27 09:49:19 +0800

    argparse

--- src/build/lib/main.py
+++ src/build/lib/main.py
@@ -1,4 +1,12 @@
 from snek import example
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--name",type=str,default="call")
+parser.add_argument("--fuck",action="store_true",default=False)
+args = parser.parse_args()
+
 
 def start():
+    print(args.name)
+    print(args.fuck)
     example.fuck()
